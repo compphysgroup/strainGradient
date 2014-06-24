dd5e97ec8d0a6c15913658d91c5e831cb33f268d
wzhenlin <wzhenlin@umich.edu>
2014-06-24 11:34:15 -0400

    comments for strainGradient.cc

--- strainGradient.cc
+++ strainGradient.cc
@@ -1,3 +1,16 @@
+/*strainGradient.cc
+ * Describe boundary value problem to solve.
+ * --------------------------
+ * Things may need modification:
+ * 1.setting parameters' values   
+ * 2.Reading NURBS geometry	  
+ * 3.applying boundary condition  
+ * 4.solver			  
+ * 5.generating output data	
+ *--------------------------
+ *June 23, 2014  
+ */
+
 //#define SOLVER_MT
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,7 +38,7 @@
 #include "parameters.h"
 using namespace std;
 
-#define DIMS 3
+#define DIMS 3 //dimention of problem
 #define NUM_QUAD_POINTS 3 //NUM_QUAD_POINTS<=5 implemented
 #define NUM_THREADS 1
 #define FINITE_STRAIN_FLAG true
@@ -45,6 +58,7 @@ private:
   void condenseKandRHS();
   void apply_initial_values();
   void mark_boundaries();
+  void mark_lines();
   void setup();
   void setupCellValues();
   void output (unsigned int _cycle);
@@ -69,6 +83,9 @@ private:
   std::ofstream energyFile;
 };
 
+/*elasticity:construction function.
+ * Define input and output parameters/variables.
+ */ 
 template <int dim>
 elasticity<dim>::elasticity (NURBSMesh<dim>& _mesh, std::string& _filePrefix, parametersClass* _params): mesh(&_mesh), filePrefix(_filePrefix), params(_params), \
 													 displacement(_mesh, NODAL, VECTOR, std::string("u")), \
@@ -77,7 +94,7 @@ elasticity<dim>::elasticity (NURBSMesh<dim>& _mesh, std::string& _filePrefix, pa
   numKnots=params->getInt("knots");
   //analysis variables
   //solution variables
-  numIncrements=1; currentIncrement=0;
+  numIncrements=1; currentIncrement=0;//define load step
   dt=1.0/numIncrements;
   //output variables
   outputVariables.push_back(&displacement);
@@ -85,6 +102,9 @@ elasticity<dim>::elasticity (NURBSMesh<dim>& _mesh, std::string& _filePrefix, pa
   outputVariables.push_back(&derivedValueR);
 }
 
+/*assemble_system
+ * Initialize assemble process for "global Stiffiness Matrix" and "right hand term".  
+ */
 template <int dim>
 void elasticity<dim>::assemble_system (){
   system_matrix=0.0; system_rhs=0.0; dU=0.0;
@@ -100,13 +120,17 @@ void elasticity<dim>::assemble_system (){
   threads.join_all ();
   printf("end assemble\n");
   printf("\nStrainEnergy:%12.4e, InterfaceEnergy:%12.6e\n", freeEnergy, interfaceEnergy);
-  condenseKandRHS();
+  condenseKandRHS();//Adjust K and RHS to reflect BC's
 }
 
+/*assemble_system_interval
+ * Assemble local matrix and local RHS by looping over every cell/element.
+ * Using algorithmic (or automatic) differentiation (AD) to linearize Residual and compute the Jacobian matrix. 
+ */
 template <int dim>
 void elasticity<dim>::assemble_system_interval (const typename std::vector<knotSpan<dim> >::iterator &begin, const typename std::vector<knotSpan<dim> >::iterator &end){
   //element loop
-  IGAValues<dim> fe_values_base(mesh, dim, 2);
+  IGAValues<dim> fe_values_base(mesh, dim, 2);//IGAValues(NURBSMesh,dofPerControlPoint,numberOfDerivatives)
   for (typename std::vector<knotSpan<dim> >::iterator cell=begin; cell<end; cell++){
     fe_values_base.reinit(*cell);
     IGAValues<dim>* fe_values=&fe_values_base;
@@ -124,6 +148,7 @@ void elasticity<dim>::assemble_system_interval (const typename std::vector<knotS
       ULocal[i].diff (i, dofs_per_cell);
       ULocalConv[i]= Un(cell->local_dof_indices[i]);
     }
+    //evaluate maximum displacement
     double maxDispLocal=0.0;
     dealii::Table<2,double> UTemp(n_q_points, dim);
     evaluateVectorFunction<double, dim>(*fe_values, 0, ULocalTemp, UTemp);
@@ -135,11 +160,11 @@ void elasticity<dim>::assemble_system_interval (const typename std::vector<knotS
       uVal=std::pow(uVal, 0.5);      
       maxDispLocal=std::max(maxDispLocal, uVal);
     }    
-    deformationMapwithGrad<Sacado::Fad::DFad<double>, dim> defMap(n_q_points); 
-    getDeformationMapWithGradient<Sacado::Fad::DFad<double>, dim>(*fe_values, 0, ULocal, defMap, currentIteration);
-    dealii::Table<1, Sacado::Fad::DFad<double> > R(dofs_per_cell); R.fill(0.0); 
+    deformationMapwithGrad<Sacado::Fad::DFad<double>, dim> defMap(n_q_points);//structural variable defined in functionEvaluations.h from IGAbase
+    getDeformationMapWithGradient<Sacado::Fad::DFad<double>, dim>(*fe_values, 0, ULocal, defMap, currentIteration);//function in mechanics.h
+    dealii::Table<1, Sacado::Fad::DFad<double> > R(dofs_per_cell); R.fill(0.0);//Residual for mechanics 
     double localfreeEnergy=0.0, localinterfaceEnergy=0.0, localdirchletBC=0.0;
-    residualForMechanics(*fe_values, 0, FINITE_STRAIN_FLAG, ULocal, ULocalConv, R, currentIteration, localfreeEnergy, localinterfaceEnergy, localdirchletBC, *cell, numKnots, params, mesh, &derivedValueP);
+    residualForMechanics(*fe_values, 0, FINITE_STRAIN_FLAG, ULocal, ULocalConv, R, currentIteration, localfreeEnergy, localinterfaceEnergy, localdirchletBC, *cell, numKnots, params, mesh, &derivedValueP);//function in mechanics.h
 			
     //Residual(R) and Jacobian(R')
     for (unsigned int i=0; i<dofs_per_cell; ++i) {
@@ -167,10 +192,14 @@ void elasticity<dim>::assemble_system_interval (const typename std::vector<knotS
   }
 }
 
+/*solve
+ * Using Newton method to solve nonlinear problem.
+ * Define parameters for Solver.
+ */
 template <int dim>
 void elasticity<dim>::solve (){
   double res=1, tol=1.0e-8, abs_tol=1.0e-11, initial_norm=0, current_norm=0;
-  currentIteration=0;
+  currentIteration=0;//interation mark in Newton method
   double oldNorm=1;
   while (true){
     if ((currentIteration>=20) or (res<1.0e-15)){printf ("Maximum number of iterations reached without convergence. \n"); break; exit (1);}
@@ -186,6 +215,7 @@ void elasticity<dim>::solve (){
     printf ("Inc:%3u, Iter:%2u. Residual norm: %10.2e. Relative norm: %10.2e, UNorm: %12.8e \n", currentIncrement, currentIteration, current_norm, res, currentNorm); 
     finalNorm=res;
     if (res<tol || current_norm< abs_tol){printf ("Residual converged in %u iterations.\n", currentIteration); break;}
+    //call solver in superLUMT_solver.h from IGAbase
     int status=luSolver(&system_matrix.nonZeroValues.at(0), &sparsity_pattern.columnIndices.at(0), &sparsity_pattern.rowIndex.at(0), &system_rhs.values.at(0), (int) system_rhs.size(), (int) system_rhs.size(), (int) sparsity_pattern.nnz, &dU.values.at(0), 0);
     if (status!=0) {printf("Solver exit status:%u\n", status); exit(-1);}
     U+=dU;
@@ -200,7 +230,10 @@ void elasticity<dim>::solve (){
   }
 }
 
-//Apply initial conditions
+/*apply_initial_values
+ * Set initial "gussing" displacement value for Newton method.
+ * Default setting is 0.
+ */
 template <int dim>
 void elasticity<dim>::apply_initial_values(){
   Un=0.0; U=0.0;
@@ -208,12 +241,20 @@ void elasticity<dim>::apply_initial_values(){
   for (typename std::vector<controlPoint<dim> >::iterator i=mesh->controlPointVector.begin(); i<mesh->controlPointVector.end(); i++){
     for (unsigned int a=0; a<dim; a++){
       displacement(i->id, a)=Un(i->id*mesh->dofPerControlPoint + a);
-      derivedValueR(i->id, a)=0.0;
+      derivedValueR(i->id, a)=0.0;//initialize output varialbe of Residual.
     }
   }
 }
 
-//Mark boundary flags
+/*mark_boundaries
+ * Mark six/four boundary surfaces.
+ * Marking Rule:
+ * X_i  boundaryFlags_Index  Default_Value  Activated_Value
+ *  0		i*2+0		0		i*2+1
+ *  1		i*2+1		0		i*2+2
+ * -------------------------------------------------------
+ * Boundary surfaces applied Neumann B.C must be activated. 
+ */
 template <int dim>
 void elasticity<dim>::mark_boundaries(){
   for (typename std::vector<knotSpan<dim> >::iterator cell=mesh->knotSpanVector.begin(); cell<mesh->knotSpanVector.end(); cell++){
@@ -224,6 +265,40 @@ void elasticity<dim>::mark_boundaries(){
   }
 }
 
+/*mark_lines
+ * Mark line that parallel to cell edge. 
+ * Following code marks twelve/four boundary edges. 
+ */
+template <int dim>
+void elasticity<dim>::mark_lines(){
+  int lineMark[2]; 
+  int Nmark=0;
+  for (typename std::vector<knotSpan<dim> >::iterator cell=mesh->knotSpanVector.begin(); cell<mesh->knotSpanVector.end(); cell++){
+    for (unsigned int i=0;i<dim; i++){
+      Nmark=0;
+      for(unsigned int j=0;j<dim;j++){
+      	if(j!=i){
+          lineMark[Nmark]=j;
+          Nmark++;
+        }
+      }
+      if(dim==3){
+      	if ((cell->endKnots[lineMark[0]][0]==0)and(cell->endKnots[lineMark[1]][0]==0)) cell->lineFlags[i*4+0]=i*4+1;
+      	if ((cell->endKnots[lineMark[0]][1]==1)and(cell->endKnots[lineMark[1]][0]==0)) cell->lineFlags[i*4+1]=i*4+2;
+      	if ((cell->endKnots[lineMark[0]][0]==0)and(cell->endKnots[lineMark[1]][1]==1)) cell->lineFlags[i*4+2]=i*4+3;
+      	if ((cell->endKnots[lineMark[0]][1]==1)and(cell->endKnots[lineMark[1]][1]==1)) cell->lineFlags[i*4+3]=i*4+4;
+      }
+     if(dim==2){
+        if (cell->endKnots[lineMark[0]][0]==0) cell->lineFlags[i*2+0]=i*2+1;
+        if (cell->endKnots[lineMark[0]][1]==1) cell->lineFlags[i*2+1]=i*2+2;
+     }
+   }
+  }
+}
+
+/*apply_boundary_conditions
+ * Following code sets dirichlet B.C of "crack" example.
+ */
 //Apply boundaries
 template <int dim>
 void elasticity<dim>::apply_boundary_conditions(){
@@ -252,6 +327,7 @@ void elasticity<dim>::apply_boundary_conditions(){
     if (coords[2]==0.0) {
        dirichletMap[controlPointDOF+2]=0.0;
     }
+    //mark specific cell and the components(x,y,z) that have variable to output.
     if ((coords[1]==0.0) and (coords[2]==0.0)){
       outputMap[coords[0]]=controlPointDOF+1;
     }
@@ -262,8 +338,9 @@ void elasticity<dim>::apply_boundary_conditions(){
   }
 }
 
-
-//Adjust K and RHS to reflect BC's
+/*condenseKandRHS
+ * Adjust K and RHS to reflect Dirichlet BC's.
+ */
 template <int dim>
 void elasticity<dim>::condenseKandRHS(){
   //Storing Residual value for output before condensing RHS
@@ -273,7 +350,7 @@ void elasticity<dim>::condenseKandRHS(){
    }
   }
   
-  //Apply dirichlet map
+  //Adjust K by applying dirichlet map which stores dof that has dirichlet BC's.   
   for (std::map<unsigned int, double>::iterator dof=dirichletMap.begin(); dof!=dirichletMap.end(); dof++){
     unsigned int i=dof->first;
     system_rhs(i)=0.0;
@@ -283,6 +360,10 @@ void elasticity<dim>::condenseKandRHS(){
   }
 }
 
+/*setupCellValues
+ * Set up fe_value of each cell.
+ * Normally no need here.
+ */
 template <int dim>
 void elasticity<dim>::setupCellValues(){
   printf("setting up cell values\n");
@@ -293,14 +374,19 @@ void elasticity<dim>::setupCellValues(){
   }
 }
 
+/*setup
+ * Initialize global data structures
+ */
 template <int dim>
 void elasticity<dim>::setup (){	
-  //initialize global data structures
   sparsity_pattern.init(mesh);
   system_matrix.reinit(sparsity_pattern);
   system_rhs.reinit(sparsity_pattern); U.reinit(sparsity_pattern); Un.reinit(sparsity_pattern); dU.reinit(sparsity_pattern); 
 }
 
+/*output
+ * Generate vts file.
+ */
 template <int dim>
 void elasticity<dim>::output (unsigned int _cycle){
   derivedValueP.projectQuadratureValues();
@@ -323,10 +409,14 @@ void elasticity<dim>::output (unsigned int _cycle){
   writeMesh<dim>(fileName, _cycle, mesh, 2*numKnots+1, outputVariables);
 }
 
+/*run
+ * 
+ */
 template <int dim>
 void elasticity<dim>::run (){
   setup();
   mark_boundaries();
+  //mark_lines();
   //setupCellValues();
   std::ofstream dataFileP, dataFileR;
   char fileNameP[200], fileNameR[200];
@@ -340,7 +430,7 @@ void elasticity<dim>::run (){
 		params->getString("order").c_str(),  \
 		params->getInt("knots"));
   dataFileP.open(fileNameP); dataFileR.open(fileNameR); 
-  double values[]={0.0, 0.01, 0.1, 1.0, 10.0};
+  double values[]={0.0, 0.01, 0.1, 1.0, 10.0};//value for l.
   for (unsigned int j=0; j<1; ++j){
     params->setBool("enforceWeakBC", (j==1)? true:false);   
     for (unsigned int i=0; i<sizeof(values)/sizeof(double); ++i){
@@ -380,6 +470,9 @@ void elasticity<dim>::run (){
   dataFileP.close(); dataFileR.close();
 }
 
+/*main
+ * 
+ */
 int main(int argc, char *argv[]){
   std::clock_t start=std::clock();
   dealii::multithread_info.n_default_threads=NUM_THREADS;
@@ -401,11 +494,11 @@ int main(int argc, char *argv[]){
   
   //read environmental variables
   printf("reading environmental variables...\n");
-  char *bcType, *order, *knots, *enforceWeakBC;
-  bcType = getenv ("bcType"); params.setString("bcType", bcType); 
-  order = getenv ("order"); params.setString("order", order);
-  knots = getenv ("knots"); params.setInt("knots", std::atoi(knots));
-  enforceWeakBC = getenv ("enforceWeakBC"); params.setBool("enforceWeakBC", (std::atoi(enforceWeakBC)==1)?"true":"false");
+  //char *bcType, *order, *knots, *enforceWeakBC;
+  //bcType = getenv ("bcType"); params.setString("bcType", bcType); 
+  //order = getenv ("order"); params.setString("order", order);
+  //knots = getenv ("knots"); params.setInt("knots", std::atoi(knots));
+  //enforceWeakBC = getenv ("enforceWeakBC"); params.setBool("enforceWeakBC", (std::atoi(enforceWeakBC)==1)?"true":"false");
 
   //NURBS file prefix
   char fileName[100];
